900d811e71a810034121ceee02bf33d5219ffd65
vovantuan2002 <votuant2@gmail.com>
2021-12-17 11:07:06 +0700

    baitap7

---
+++ bai7CleanCode&Refactoring/Refactoring.php
@@ -0,0 +1,35 @@
+<?php
+const ADDITION = "+";
+const SUBTRACTION = "-";
+const MULTIPLICATION = "*";
+const DIVISION = "/";
+class Calculator{
+    public function Calculator($firstOperand, $secondOperand, $operator) : float|int{
+        switch ($operator){
+            case ADDITION;
+            return $firstOperand + $secondOperand;
+            case SUBTRACTION;
+            return $firstOperand - $secondOperand;
+            case MULTIPLICATION;
+            return $firstOperand * $secondOperand;
+            case DIVISION;
+            if ($secondOperand != 0){
+            return $firstOperand / $secondOperand;
+            }else{
+                return "Can not divide by 0";
+            }
+            default;
+            return "Unsupported operation";
+        }
+    }
+}
+
+$calculator = new Calculator();
+echo $calculator->Calculator(1,2, '-');
+echo "<br>";
+echo $calculator->Calculator(1,2, '+');
+echo "<br>";
+echo $calculator->Calculator(1,2, '*');
+echo "<br>";
+echo $calculator->Calculator(1,0, '/');
+?>
---
+++ bai7CleanCode&Refactoring/refscttong_con.php
@@ -0,0 +1,7 @@
+<?php
+
+include_once "Refactoring_tach_bien.php";
+
+$fizzBuzz = new FizzBuzz(888);
+
+echo $fizzBuzz;
